a5dae9965c69d1da4ee2089422b5381e4d39bb9c
KRHero <krunalhero@gmail.com>
2019-03-20 12:44:49 +0530

    Starting Project

--- app/src/main/java/com/kr/pineco/MainActivity.java
+++ app/src/main/java/com/kr/pineco/MainActivity.java
@@ -2,12 +2,37 @@ package com.kr.pineco;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    Button mainActivityLogin,mainActivitySignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainActivityLogin=findViewById(R.id.mainActivityLogin);
+        mainActivitySignUp=findViewById(R.id.mainActivitySignUp);
+
+        mainActivityLogin.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mainActivitySignUp.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
+
+
 }
